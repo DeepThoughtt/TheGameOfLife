eddd0541537b5c0b322ee30677f31d391566717d
Deep Thought <50927069+DeepThoughtt@users.noreply.github.com>
2023-09-12 17:10:19 +0200

    Update game_of_life.cpp

--- cpp/game_of_life.cpp
+++ cpp/game_of_life.cpp
@@ -12,7 +12,7 @@ void initialize_game_grid(int grid[GAME_SIZE][GAME_SIZE])
 {
 	int i = 0;
 	
-	while(i < GAME_SIZE)
+	while (i < GAME_SIZE)
 	{
 		grid[0][i] = -1;
 		grid[i][0] = -1;
@@ -25,8 +25,8 @@ void initialize_game_grid(int grid[GAME_SIZE][GAME_SIZE])
 
 void reset_game_grid(int grid[GAME_SIZE][GAME_SIZE])
 {	
-	for(int i = 0; i < GAME_SIZE; i++)
-		for(int j = 0; j < GAME_SIZE; j++)
+	for (int i = 0; i < GAME_SIZE; i++)
+		for (int j = 0; j < GAME_SIZE; j++)
 			grid[i][j] = 0;
 		
 	initialize_game_grid(grid);
@@ -38,14 +38,14 @@ void print_game_grid(int grid[GAME_SIZE][GAME_SIZE])
 	using std::cout;
 	using std::endl;
 	
-	for(int i = 0; i < GAME_SIZE; i++)
+	for (int i = 0; i < GAME_SIZE; i++)
 	{
-		for(int j = 0; j < GAME_SIZE; j++)
+		for (int j = 0; j < GAME_SIZE; j++)
 		{
-			if(grid[i][j] == 0)
+			if (grid[i][j] == 0)
 				cout << "  ";
 			
-			else if(grid[i][j] > 0)
+			else if (grid[i][j] > 0)
 				cout << "#" << " ";
 			
 			else
@@ -58,8 +58,8 @@ void print_game_grid(int grid[GAME_SIZE][GAME_SIZE])
 
 void reset_data_grid(int grid[GAME_SIZE - 2][GAME_SIZE - 2])
 {	
-	for(int i = 0; i < GAME_SIZE - 2; i++)
-		for(int j = 0; j < GAME_SIZE - 2; j++)
+	for (int i = 0; i < GAME_SIZE - 2; i++)
+		for (int j = 0; j < GAME_SIZE - 2; j++)
 			grid[i][j] = 0;
 }
 
@@ -69,9 +69,9 @@ void print_data_grid(int grid[GAME_SIZE - 2][GAME_SIZE - 2])
 	using std::cout;
 	using std::endl;
 	
-	for(int i = 0; i < GAME_SIZE - 2; i++)
+	for (int i = 0; i < GAME_SIZE - 2; i++)
 	{
-		for(int j = 0; j < GAME_SIZE - 2; j++)
+		for (int j = 0; j < GAME_SIZE - 2; j++)
 			cout << grid[i][j] << " ";
 		
 		cout << endl;
@@ -83,8 +83,8 @@ int data_sum(int grid[GAME_SIZE - 2][GAME_SIZE - 2])
 {
 	int sum = 0;
 	
-	for(int i = 0; i < GAME_SIZE - 2; i++)
-		for(int j = 0; j < GAME_SIZE - 2; j++)
+	for (int i = 0; i < GAME_SIZE - 2; i++)
+		for (int j = 0; j < GAME_SIZE - 2; j++)
 			sum += grid[i][j];
 		
 	return sum;
@@ -93,16 +93,16 @@ int data_sum(int grid[GAME_SIZE - 2][GAME_SIZE - 2])
 
 void update_data_grid_values(int data_grid[GAME_SIZE - 2][GAME_SIZE - 2], int game_grid[GAME_SIZE][GAME_SIZE])
 {	
-	for(int i = 1; i < GAME_SIZE - 1; i++)
+	for (int i = 1; i < GAME_SIZE - 1; i++)
 	{
-		for(int j = 1; j < GAME_SIZE - 1; j++)
+		for (int j = 1; j < GAME_SIZE - 1; j++)
 		{
 			int active_neighbors = 0;
 				
-			if(game_grid[i + 1][j] > 0)
+			if (game_grid[i + 1][j] > 0)
 				active_neighbors++;
 			
-			if(game_grid[i - 1][j] > 0)
+			if (game_grid[i - 1][j] > 0)
 				active_neighbors++;
 				
 			int x_index = i - 1;
@@ -111,12 +111,12 @@ void update_data_grid_values(int data_grid[GAME_SIZE - 2][GAME_SIZE - 2], int ga
 				
 			int loop_check = 0;
 				
-			while(loop_check < 3)
+			while (loop_check < 3)
 			{
-				if(game_grid[x_index][y_index_1] > 0)
+				if (game_grid[x_index][y_index_1] > 0)
 					active_neighbors++;	
 					
-				if(game_grid[x_index][y_index_2] > 0)
+				if (game_grid[x_index][y_index_2] > 0)
 					active_neighbors++;
 					
 				x_index++;
@@ -132,36 +132,36 @@ void update_data_grid_values(int data_grid[GAME_SIZE - 2][GAME_SIZE - 2], int ga
 void update_game_grid_values(int game_grid[GAME_SIZE][GAME_SIZE], int data_grid[GAME_SIZE - 2][GAME_SIZE - 2], int function = 2)
 {
 	// during the initialization we don't want to elaborate who lives and who dies
-	if(function == 1)
+	if (function == 1)
 	{
-		for(int i = 1; i < GAME_SIZE - 1; i++)
-			for(int j = 1; j < GAME_SIZE - 1; j++)
+		for (int i = 1; i < GAME_SIZE - 1; i++)
+			for (int j = 1; j < GAME_SIZE - 1; j++)
 			{
-				if(data_grid[i - 1][j - 1] == 1)
+				if (data_grid[i - 1][j - 1] == 1)
 					game_grid[i][j] = 1;
 			}
 			
 		return;
 	}
 	
-	if(function == 2)
+	if (function == 2)
 	{
-		for(int i = 1; i < GAME_SIZE - 1; i++)
+		for (int i = 1; i < GAME_SIZE - 1; i++)
 		{
-			for(int j = 1; j < GAME_SIZE - 1; j++)
+			for (int j = 1; j < GAME_SIZE - 1; j++)
 			{
 				// OVERPOPULATION
-				if(data_grid[i - 1][j - 1] > 3)
+				if (data_grid[i - 1][j - 1] > 3)
 					game_grid[i][j] = 0;			
 				
 				// ISOLATION
-				if(data_grid[i - 1][j - 1] < 2)
+				if (data_grid[i - 1][j - 1] < 2)
 					game_grid[i][j] = 0;					
 				
-				if(data_grid[i - 1][j - 1] == 2 || data_grid[i - 1][j - 1] == 3)
+				if (data_grid[i - 1][j - 1] == 2 || data_grid[i - 1][j - 1] == 3)
 				{
 					// REPRODUCTION
-					if(data_grid[i - 1][j - 1] == 3)
+					if (data_grid[i - 1][j - 1] == 3)
 						game_grid[i][j] = data_grid[i - 1][j - 1];	
 				}
 			}
@@ -178,19 +178,19 @@ int load_data(std::string filename, int data_grid[GAME_SIZE - 2][GAME_SIZE - 2],
 	
 	input_stream.open(final_filename.c_str());
 	
-	if(input_stream.fail())
+	if (input_stream.fail())
 	{
 		input_stream.close();
 		return 1;
 	}
 	
-	for(int i = 0; i < GAME_SIZE - 2; i++)
-		for(int j = 0; j < GAME_SIZE - 2; j++)
+	for (int i = 0; i < GAME_SIZE - 2; i++)
+		for (int j = 0; j < GAME_SIZE - 2; j++)
 		{
 			int initial = 0;
 			input_stream >> initial;
 			
-			if(initial == 1)
+			if (initial == 1)
 				data_grid[i][j] = initial;
 		}
 		
@@ -205,9 +205,9 @@ void update_log_file(int data_grid[GAME_SIZE - 2][GAME_SIZE - 2], int& iteration
 	std::ofstream output_stream;
 	output_stream.open("./log/logfile.txt", std::ios_base::app);
 	
-	for(int i = 0; i < GAME_SIZE - 2; i++)
+	for (int i = 0; i < GAME_SIZE - 2; i++)
 	{
-		for(int j = 0; j < GAME_SIZE - 2; j++)
+		for (int j = 0; j < GAME_SIZE - 2; j++)
 			output_stream << data_grid[i][j] << " ";
 		
 		output_stream << "\n";
@@ -247,7 +247,7 @@ int main()
 	cout << "Do you want to upload extern data (y/n)? ";
 	cin >> choice;
 	
-	while(choice != 'y' && choice != 'n' || cin.fail())
+	while (choice != 'y' && choice != 'n' || cin.fail())
 	{
 		cin.clear();
 		cin.ignore();
@@ -255,7 +255,7 @@ int main()
 		cin >> choice;
 	}
 	
-	if(choice == 'y')
+	if (choice == 'y')
 	{
 		std::string filename;
 		cin.ignore();
@@ -264,12 +264,12 @@ int main()
 		int status = 1;
 		
 		status = load_data(filename, data_grid, game_grid);
-		while(status == 1)
+		while (status == 1)
 		{
 			cout << "Unable to find the file, do you want to retry (y/n)? ";
 			cin >> choice;
 			
-			while(choice != 'y' && choice != 'n')
+			while (choice != 'y' && choice != 'n')
 			{
 				cin.clear();
 				cin.ignore();
@@ -277,7 +277,7 @@ int main()
 				cin >> choice;
 			}
 			
-			if(choice == 'y')
+			if (choice == 'y')
 			{
 				cin.ignore();
 				cout << "Filename: ";
@@ -285,14 +285,14 @@ int main()
 				status = load_data(filename, data_grid, game_grid);
 			}
 			
-			if(choice == 'n')
+			if (choice == 'n')
 			{
 				cout << "Upload aborted\n";
 				break;
 			}
 		}
 		
-		if(status == 0)
+		if (status == 0)
 		{
 			cout << "Upload successful\n";
 			print_game_grid(game_grid);
@@ -303,30 +303,30 @@ int main()
 	int input_y = 0;
 	
 	// starting configuration
-	while(input_x >= 0 && input_y >= 0)
+	while (input_x >= 0 && input_y >= 0)
 	{
 		cout << "Insert data (between 0 and " << GAME_SIZE - 3 << "):\n>> Insert a number lower than 0 to stop inserting data \n";
 		
 		cout << "X: ";
 		cin >> input_x;
 		
-		if(input_x < 0)
+		if (input_x < 0)
 			break;
 		
 		cout << "Y: ";
 		cin >> input_y;
 		
-		if(input_y < 0)
+		if (input_y < 0)
 			break;
 		
-		if(input_x >= 0 && input_x < GAME_SIZE - 2 && input_y >= 0 && input_y < GAME_SIZE - 2)
+		if (input_x >= 0 && input_x < GAME_SIZE - 2 && input_y >= 0 && input_y < GAME_SIZE - 2)
 		{
 			data_grid[input_y][input_x] = 1;
 			update_game_grid_values(game_grid, data_grid, 1);
 			print_game_grid(game_grid);
 		}
 		
-		else if(cin.fail() || input_x >= GAME_SIZE - 2 || input_y >= GAME_SIZE - 2)
+		else if (cin.fail() || input_x >= GAME_SIZE - 2 || input_y >= GAME_SIZE - 2)
 		{
 			cin.clear();
 			cin.ignore();
@@ -338,7 +338,7 @@ int main()
 	cout << "Do you want to create a log file (y/n)? ";
 	cin >> logfile_needed;
 	
-	while(logfile_needed != 'y' && logfile_needed != 'n' || cin.fail())
+	while (logfile_needed != 'y' && logfile_needed != 'n' || cin.fail())
 	{
 		cin.clear();
 		cin.ignore();
@@ -348,10 +348,10 @@ int main()
 	
 	int iterations = 0;
 	
-	if(logfile_needed == 'y')
+	if (logfile_needed == 'y')
 		update_log_file(data_grid, iterations);
 	
-	while(data_sum(data_grid) > 0)
+	while (data_sum(data_grid) > 0)
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(GAME_MILLISECONDS));
 		
@@ -359,7 +359,7 @@ int main()
 		update_game_grid_values(game_grid, data_grid);
 		print_game_grid(game_grid);
 		
-		if(logfile_needed == 'y')
+		if (logfile_needed == 'y')
 			update_log_file(data_grid, iterations);
 	}
 	
